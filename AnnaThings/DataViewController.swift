17a5f342061a88ecc3c9e73bcea23549229d57f9
陈栋 <dchen1@vova.com.hk>
2019-04-29 18:17:42 +0800

    genxin

--- AnnaThings/DataViewController.swift
+++ AnnaThings/DataViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SnapKit
 import Charts
+import SwiftDate
 
 class DataViewController: UIViewController {
     
@@ -24,22 +25,20 @@ class DataViewController: UIViewController {
     func setUpView(){
         view.backgroundColor = UIColor.white
         
-        
-        let seg = UISegmentedControl(items: [Incident.DataTimeIntervalType.Day.rawValue,
-                                             Incident.DataTimeIntervalType.Week.rawValue,
-                                             Incident.DataTimeIntervalType.Month.rawValue])
+        // segment
+        let seg = UISegmentedControl(items: [IncidentData.DataTimeIntervalType.Day.rawValue,
+                                             IncidentData.DataTimeIntervalType.Week.rawValue,
+                                             IncidentData.DataTimeIntervalType.Month.rawValue])
         view.addSubview(seg)
-        
         seg.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(74)
             make.leading.equalToSuperview().offset(10)
             make.trailing.equalToSuperview().offset(-10)
         }
-        
         seg.selectedSegmentIndex = 1;
-        
         seg.addTarget(self, action: #selector(tapSeg(_:)), for: .valueChanged)
         
+        // chartView
         chartView = LineChartView(frame: CGRect.zero)
         view.addSubview(chartView)
         chartView.snp.makeConstraints { (make) in
@@ -47,17 +46,72 @@ class DataViewController: UIViewController {
             make.top.equalTo(seg.snp.bottom).offset(10)
             make.height.equalTo(200)
         }
+
+        let period = TimePeriod(end: DateInRegion(), duration: 1.months + 2.weeks)
         
-        let dic : [String: [Incident]] = incidentsSeprateBy(intervelType: .Week)
+        let sd = IncidentData().incidentInTimePeriod(peroid: period,
+                                                     type: .Week)
         
-        let yVals = (0...6).map { (i) -> BarChartDataEntry in
-            
-            return BarChartDataEntry(x: Double(i), y: 1)
-        }
+        
+//        let res = incidentsSeprateBy(intervelType: .Week)
+        
+//        let dic : [String: [Incident]] = res.1
+//
+//        let boundary:(Incident?, Incident?) = res.0
+//
+//        guard let start = boundary.0, let end = boundary.1 else {
+//            return
+//        }
+//
+//        // 开始日期
+//        let ss = start.time.dateAt(.startOfWeek)
+//
+//        // 结束日期
+//        let ee = end.time.dateAt(.endOfWeek)
+//
+//        var bol = false
+//        var currentDate = ss
+//        var dates = [Date]()
+//        repeat {
+//            dates.append(currentDate)
+//            let next = currentDate.dateAt(.tomorrow)
+//            currentDate = next
+//            bol = next.isBeforeDate(ee, orEqual: true, granularity: .day)
+//        } while bol
+//
+//        let yVals = (0..<dates.count).map { (i) -> BarChartDataEntry in
+//
+//            return BarChartDataEntry(x: Double(i), y: 1)
+//        }
     }
     
     
     @objc func tapSeg(_ seg: UISegmentedControl) {
         
     }
+}
+
+
+class HorizontalCollectionView: UICollectionView {
+    class cell: UICollectionViewCell {
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+    }
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        super.init(frame: CGRect.zero, collectionViewLayout: layout)
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
 }
--- AnnaThings/Models.swift
+++ AnnaThings/Models.swift
@@ -9,27 +9,53 @@
 import RealmSwift
 import SwiftDate
 
+// MARK: - 首页
 func latestIncident(type: Incident.IncidentType) -> Incident? {
     let res = realm().objects(Incident.self).filter("type = %@", type.rawValue).sorted(byKeyPath: "time").last
     return res
 }
 
-func incidentsSeprateBy(intervelType: Incident.DataTimeIntervalType) -> [String: [Incident]]{
+// MARK: - 数据页
+struct IncidentData {
     
-    let res = realm().objects(Incident.self)
+    enum DataTimeIntervalType: String {
+        case Month = "月"
+        case Week = "周"
+        case Day = "日"
+    }
     
-    return Dictionary(grouping: res) { (ele) -> String in
+    private var incidents : Results<Incident>!
+    init() {
+        incidents = realm().objects(Incident.self).sorted(byKeyPath: "time")
+    }
     
-        switch intervelType {
-        case .Day:
-            return ele.time.dateAt(DateRelatedType.startOfDay).toFormat(Incident.DateFormat)
-        case .Month:
-            return ele.time.dateAt(DateRelatedType.startOfMonth).toFormat(Incident.DateFormat)
-        case .Week:
-            return ele.time.dateAt(DateRelatedType.startOfWeek).toFormat(Incident.DateFormat)
+    var incidentBoundary : (Incident?, Incident?) {
+        get {
+            return (incidents.first, incidents.last)
         }
     }
+    
+    static let DateFormat = "yyyy-MM-dd-HH-mm"
+    
+    func incidentInTimePeriod(peroid timePeroid: TimePeriod, type: DataTimeIntervalType) -> [String: [Incident]]{
+        
+        let res = incidents.filter("time >= %@ and time =< %@", timePeroid.start!.date, timePeroid.end!.date)
+        
+        let dic = Dictionary(grouping: res) { (ele) -> String in
+            switch type {
+            case .Day:
+                return ele.time.dateAt(.startOfDay).toFormat(IncidentData.DateFormat)
+            case .Month:
+                return ele.time.dateAt(.startOfMonth).toFormat(IncidentData.DateFormat)
+            case .Week:
+                return ele.time.dateAt(.startOfWeek).toFormat(IncidentData.DateFormat)
+            }
+        }
+        return dic
+    }
 }
+
+// MARK: - 数据
 
 private func realm() -> Realm{
     let rl = try! Realm()
@@ -42,14 +68,6 @@ class Incident: Object {
         case pee
         case pupu
     }
-    
-    enum DataTimeIntervalType: String {
-        case Month = "月"
-        case Week = "周"
-        case Day = "日"
-    }
-    
-    static let DateFormat = "yyyy-MM-dd-HH-mm"
     
     @objc dynamic var time: Date = Date()
     @objc dynamic var type: Int8 = IncidentType.pee.rawValue
